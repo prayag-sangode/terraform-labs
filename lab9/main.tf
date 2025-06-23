662fff278d78289cb389941283c24d52d2cdd136
Prayag Sangode <prayag.rhce@gmail.com>
2025-06-23 09:14:41 +0000

    update count lab

---
+++ lab9/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami             = "ami-0c94855ba95c71c99"
+  instance_type   = "t2.micro"
+  count           = 2
+
+  tags = {
+    Name = "example-instance-${count.index}"
+  }
+}
